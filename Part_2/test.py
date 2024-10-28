af0d1edabb4c76db33c482e0aa67145e638b5ea3
Serega Petrovsky <petrovsky.dev@gmail.com>
2024-10-28 19:11:40 +0300

    create file 2.20

---
+++ Part_2/test.py
@@ -0,0 +1,11 @@
+name = 'Jony'
+print(f'Привет, {name}.')
+
+temperature = 45
+print(f'Сейчас температура составляет {temperature} градусов')
+
+
+print(f'значение равно {10 + 2}')
+
+val = 18
+print(f'значение равно {val + 2}')
